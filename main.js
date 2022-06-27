6101b2d26ec91ea7ea5ef27455171aab1368e819
Anik <sikder35-1408@diu.edu.bd>
2022-06-27 11:31:37 +0600

    Mobile menu with js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const mobileMenu = document.querySelector('.mobile__menu');
+const navMenu = document.querySelector('.navigation');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.add('show__menu');
+})
+
+navMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show__menu');
+})
